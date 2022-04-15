90435119c419d6f21c19cbb444f1f22fceafce58
Shrushti Polekar <shrushtipolekar@gmail.com>
2022-04-15 16:20:33 +0530

    style:changed btn color

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -9,7 +9,7 @@ const Header = () => {
     <>
       <div class="navigation-container flex-hz">
         <div className="navigation-item-1 flex-hz jc-sb">
-          <h5 class="lib-name-1">Glowing <span class="lib-name-2">Divas</span></h5>
+          <Link className='links' to="/"><h5 class="lib-name-1">Glowing <span class="lib-name-2">Divas</span></h5></Link>
           <Link to="/" className='links header-link'>Explore </Link>
           <Link to="/" className='links header-link'>Playlist </Link>
         </div>
