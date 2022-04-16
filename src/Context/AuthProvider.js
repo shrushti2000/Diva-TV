23ef373d298dc6ef5976b04c09f1988c9a0bf5d0
Shrushti Polekar <shrushtipolekar@gmail.com>
2022-04-16 19:05:43 +0530

    refactor: refactored imports

--- src/Context/AuthProvider.js
+++ src/Context/AuthProvider.js
@@ -1,7 +1,5 @@
 import React from 'react'
-import { createContext } from 'react'
-import { useContext } from 'react'
-import { useState } from 'react'
+import { createContext,useContext,useState } from 'react'
 
 const AuthContext = createContext()
 
